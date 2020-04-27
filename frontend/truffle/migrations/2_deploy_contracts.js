a7981057b54e66c33098388e5ad9c6242d0f641e
francisco279 <francisco_aleman09@hotmail.com>
2020-04-27 17:26:09 -0500

    Adding a json validator

--- frontend/truffle/migrations/2_deploy_contracts.js
+++ frontend/truffle/migrations/2_deploy_contracts.js
@@ -2,10 +2,12 @@ const PatientDiagnosis = artifacts.require("PatientDiagnosis");
 const MedicsRegister = artifacts.require("MedicsRegister");
 const AssistantRegister = artifacts.require("AssistantRegister");
 const PendingRecords = artifacts.require("PendingRecords");
+const Validator = artifacts.require("Validator");
 
 module.exports = function(deployer) {
     deployer.deploy(PatientDiagnosis, "Historial Clínico Electrónico");
     deployer.deploy(MedicsRegister, "Historial Clínico Electrónico");
     deployer.deploy(AssistantRegister, "Historial Clínico Electrónico");
     deployer.deploy(PendingRecords, "Historial Clínico Electrónico");
+    deployer.deploy(Validator, "Historial Clínico Electrónico");
 }
