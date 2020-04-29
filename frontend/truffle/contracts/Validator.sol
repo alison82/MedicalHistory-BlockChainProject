97d023e13f9100f41414e570a3c050753d2c1220
Alison Muñoz Capote <43675573+alison82@users.noreply.github.com>
2020-04-28 20:10:52 -0500

    update contracts and validator.sol

--- frontend/truffle/contracts/Validator.sol
+++ frontend/truffle/contracts/Validator.sol
@@ -1,19 +1,9 @@
-pragma solidity >=0.4.21 <0.7.0
+pragma solidity 0.5.16;
 
 contract Validator {
-    
+
     address owner;
     mapping(uint256=>bool) usedNonces;
-    
-    constructor()public{
-        owner = msg.sender;
-    }
-    
-    modifier onlyOwner(){
-        if(owner == msg.sender){
-            _;
-        }
-    }
 
     function verifyNonce(uint256 Nonce)public returns(bool success){
         require(!usedNonces[Nonce], "Nonce used");
@@ -22,12 +12,12 @@ contract Validator {
         success = true;
     }
 
-    function verify(bytes32 r, bytes32 s, uint8 v, bytes32 hash)public returns(address) {
+    function verify(bytes32 r, bytes32 s, uint8 v, bytes32 hash)public pure returns(address) {
         return ecrecover(hash, v, r, s);
-    } 
-    
-   
-    
+    }
+
+
+
     // function kill() onlyOwner {
     //     suicide(owner);
     // }
